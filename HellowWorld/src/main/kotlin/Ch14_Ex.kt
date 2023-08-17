4aac054a84111cea1442f1a74536dc50e5436d87
Ming goo <130649685+llkkmm0823@users.noreply.github.com>
2023-08-17 14:53:03 +0900

    211111

---
+++ HellowWorld/src/main/kotlin/Ch14_Ex.kt
@@ -0,0 +1,35 @@
+fun main() {
+    print("연도 입력: ")
+    val year: Int = readLine()!!.toInt()
+    print("월 입력: ")
+    val month: Int = readLine()!!.toInt()
+
+    val first = getFirstDayOfMonth(year, month)
+    val day = getDaysInMonth(year, month)
+
+    println("일 월 화 수 목 금 토")
+
+    for (i in 1 until first) {
+        print("   ")
+    }
+
+    for (day in 1..day) {
+        print("${day.toString().padStart(2, ' ')} ")
+        if ((first + day - 1) % 7 == 0) {
+
+            println()
+        }
+    }
+}
+
+fun getFirstDayOfMonth(year: Int, month: Int): Int {
+    val calendar = java.util.Calendar.getInstance()
+    calendar.set(year, month - 1, 1)
+    return calendar.get(java.util.Calendar.DAY_OF_WEEK)
+}
+
+fun getDaysInMonth(year: Int, month: Int): Int {
+    val calendar = java.util.Calendar.getInstance()
+    calendar.set(year, month - 1, 1)
+    return calendar.getActualMaximum(java.util.Calendar.DAY_OF_MONTH)
+}
